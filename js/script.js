c0bc084792d29a896fbff1c3cdec3c64e3ce5122
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-11 23:42:00 +0200

    page-login

--- js/script.js
+++ js/script.js
@@ -30,22 +30,3 @@ const valid = function fun1() {
 }
 btn.addEventListener('click', valid);
 
-const btn2 = document.querySelector('#throw');
-
-const play = function fun2(){
-    let max = 6;
-    let min = 1;
-    const player = Math.floor(Math.random() * (max - min) ) + min;
-    const pc = Math.floor(Math.random() * (max - min) ) + min;
-    let span = document.querySelector('#result-game');
-    span.innerHTML = 'Dado player: ' + player + ' Dado pc: ' + pc;
-    if(player == pc){
-        span.innerHTML += ' Pareggio!';
-    }else if(player > pc){
-        span.innerHTML += ' Hai Vinto!';
-    }
-    else{
-        span.innerHTML += ' Hai Perso!';
-    }
-}
-btn2.addEventListener('click', play);
